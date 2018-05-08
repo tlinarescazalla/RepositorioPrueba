e08e90d404ad14f4978d7226b3d604bd7d75198f
Teresa Linares <tlinares@mi.ad>
2018-05-08 10:29:23 +0200

    TereCommit

---
+++ PracticasCursoSelenium/Questions.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticasCursoSelenium
+{
+    class Questions
+    {
+        IWebDriver vChrome;
+        private string Url;
+        private string username;
+        private string password;
+        private string key;
+
+        [SetUp]
+        public void SetUpBrowserVariables()
+        {
+            try
+            {
+                //Setting Navegador
+                vChrome = new ChromeDriver();
+                vChrome.Manage().Window.Maximize();
+                vChrome.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+                //Setting variables
+                Url = "https://examdevtst1.pearsonvue.com/ExamDeveloper/login/Login.aspx";
+                username = "Auto1";
+                password = "ed1tHPi0f";
+                key = "velvet";
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [Test]
+        public void CreateQuestion()
+        {
+            try
+            {
+                //Navegamos a ST1
+                vChrome.Navigate().GoToUrl(Url);
+
+                //Login
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).Click();
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).Clear();
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbUsername")).SendKeys(username);
+
+                //Password
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).Click();
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).Clear();
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_tbPassword")).SendKeys(password);
+
+                //Click Next
+                vChrome.FindElement(By.Id("ctl00_RHSContentPlaceHolder_btnLogin")).Click();
+
+                //Velvet
+                vChrome.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).Click();
+                vChrome.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).Clear();
+                vChrome.FindElement(By.Id("ctl00_ctl00_RHSContentPlaceHolder_RHSContentPlaceHolder_tbAnswer")).SendKeys(key);
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            vChrome.Quit();
+        }
+    }
+}
